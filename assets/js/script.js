1f0a153446b9e4c48fc4fda07c9c7303c4b9919d
DJVegaG <domingo.vega1901@alumnos.ubiobio.cl>
2024-03-12 18:32:48 -0300

    Añadiendo comentarios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
+    // Efecto de desplazamiento suave al hacer clic en los enlaces del navbar
     $("a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
-
             var hash = this.hash;
-
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function () {
@@ -13,5 +12,6 @@ $(document).ready(function () {
         }
     });
 
+    // Tooltip para utilizar en los campos de relleno del formulario de contacto
     $('[data-toggle="tooltip"]').tooltip();
 });
